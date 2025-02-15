25c742e6922b3f372bf6ac3d0754851dd8b8aeec
Mrinmoy Koiri <153649837+MK10UNoY@users.noreply.github.com>
2025-02-15 10:37:27 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,22 @@
-<script>
-	import Home from '$lib/Home.svelte';
-	import About from '$lib/About.svelte';
-	import Projects from '$lib/Projects.svelte';
-	import Contact from '$lib/Contact.svelte';
-	import Footer from '$lib/Footer.svelte';
-</script>
+//<script>
+//	import Home from '$lib/Home.svelte';
+//	import About from '$lib/About.svelte';
+//	import Projects from '$lib/Projects.svelte';
+//	import Contact from '$lib/Contact.svelte';
+//	import Footer from '$lib/Footer.svelte';
+//</script>
 
-<Home />
-<About />
-<Projects />
-<Contact />
-<Footer />
+//<Home />
+//<About />
+//<Projects />
+//<Contact />
+//<Footer />
+
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	onMount(() => {
+		goto('/newpage'); // Replace with your actual route
+	});
+</script>
